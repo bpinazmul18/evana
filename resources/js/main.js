944429e0f8939b770fd5ba2d078a7c19e266b5c1
Md Nazmul Haquee <bpinazmul18@gmail.com>
2020-11-16 19:27:32 +0600

    unknown update

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+    $(".js--about-section").waypoint(function(direction){
+        if(direction == "down"){
+            $("nav").addClass("sticky");
+        }else{
+            $("nav").removeClass("sticky");
+        }
+    });
+});
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
@@ -5,3 +14,4 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
